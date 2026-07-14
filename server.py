62060868cb1e124b02d0ff343b0b7039b60a2cbb
Aruvenos <sanyasfgfgf@gmail.com>
2026-07-14 22:12:04 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+@app.route("/secret")
+def autoriz():
+    return render_template("main/secret.html")
+
+@app.route("/post", methods=["POST"])
+def post():
+    data = request.get_json()
+    print(data)
+    user = data["user"]
+    password = data["password"]
+    if user == "admin" and password == "password":
+        return jsonify({"is": "OK", "site": "secret/sobaka"}), 200
+    else:
+        return jsonify({"is": "NO", "site": "/secret"}), 200
+    
+@app.route("/secret/sobaka")
+def hidden():
+    return render_template("secret/sobaka.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
